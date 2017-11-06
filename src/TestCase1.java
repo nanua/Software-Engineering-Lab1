771b7e16e9e79d1a74a8e9049e75d20004a0ea10
IceCreamWW <1450648595@qq.com>
2017-11-06 15:50:53 +0800

    Lab6 white box

---
+++ src/TestCase1.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class TestGraph {
+    @Test
+    public void testQueryBridgeWords(){
+
+    }
+    @Test
+    public void testQueryBridgeWords2(){
+
+    }
+    @Test
+    public void testQueryBridgeWords3(){
+
+    }
+    @Test
+    public void testQueryBridgeWords4(){
+
+    }
+
+}
